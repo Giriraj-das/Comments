9d0d35df73f9db62d1784843306cc8e66d436317
Oleksandr Radchenko <oradchenko108@gmail.com>
2025-01-31 20:03:44 +0530

    fixed display of avatars

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,15 +1,15 @@
 from django.db import models
+from .validators import AvatarValidator, FileValidator
 
 
 class Comment(models.Model):
     username = models.CharField(max_length=50)
-    avatar_url = models.URLField(null=True, blank=True)
     email = models.EmailField()
     home_page = models.URLField(blank=True, null=True)
     text = models.TextField()
     parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
-    image = models.ImageField(upload_to='images/', blank=True, null=True)
-    file = models.FileField(upload_to='files/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+    file = models.FileField(upload_to='images_files/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
